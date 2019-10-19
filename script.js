adfe1dfd2514e8c7e5c192efc98ee5e1eca1d6e1
vinnav <54999963+vinnav@users.noreply.github.com>
2019-10-19 18:25:59 +0100

    Added time/break logic and button functions

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ upSession = document.getElementById("upSession");
 upSession.addEventListener("click", upSessionFunction);
 downSession = document.getElementById("downSession");
 downSession.addEventListener("click", downSessionFunction);
-let sessionMinutes = 25;
+let sessionMinutes = 25; // put 25
 
 function upSessionFunction(){
     if(sessionMinutes<60){
@@ -14,13 +14,15 @@ function upSessionFunction(){
     }
     document.getElementById("sTime").innerHTML = sessionMinutes + ":00";
     document.getElementById("time").innerHTML = sessionMinutes + ":00";
+    timeleft = (sessionMinutes*60)-61;
 }
 function downSessionFunction(){
-    if(sessionMinutes>0){
+    if(sessionMinutes>5){ // put 5 minimum
         sessionMinutes--;
     }
     document.getElementById("sTime").innerHTML = sessionMinutes + ":00";
     document.getElementById("time").innerHTML = sessionMinutes + ":00";
+    timeleft = (sessionMinutes*60)-61;
 }
     // Break Time
 unBreak = document.getElementById("upBreak");
@@ -35,29 +37,124 @@ function upBreakFunction(){
     document.getElementById("bTime").innerHTML = breakMinutes + ":00";
 }
 function downBreakFunction(){
-    if(breakMinutes>0){
+    if(breakMinutes>2){
         breakMinutes--;
     }
     document.getElementById("bTime").innerHTML = breakMinutes + ":00";
 }
 
-//other function
+//Logic
+let timeStatus = 0; // 0 is Time, 1 is Break
 
 
 
 //Play
 var timeleft = 1439; // 25 minutes default - 1439
-play = document.getElementById("play");
+let play = document.getElementById("play");
 play.addEventListener("click", timeCountdown);
 
+
 function timeCountdown(){
-    timeleft = (sessionMinutes*60)-61;
-var downloadTimer = setInterval(function(){
-  document.getElementById("time").innerHTML = Math.round(timeleft/60) + ":" + Math.round(timeleft%60);
-  timeleft -= 1;
-  if(timeleft <= 0){
-    clearInterval(downloadTimer);
-    document.getElementById("time").innerHTML = "Finished"
-  }
-}, 1000);
+    upSession.removeEventListener("click", upSessionFunction);
+    downSession.removeEventListener("click", downSessionFunction);
+    upBreak.removeEventListener("click", upBreakFunction);
+    downBreak.removeEventListener("click", downBreakFunction);
+    play.removeEventListener("click", timeCountdown);
+    downTimer = setInterval(function(){
+    document.getElementById("time").innerHTML = Math.floor(timeleft/60) + ":" + Math.floor(timeleft%60);
+    timeleft--;
+
+
+    if(timeleft <= 0){
+        clearInterval(downTimer);
+            if(timeStatus == 0){
+                document.getElementById("time").innerHTML = "Start Break";
+                timeleft = (breakMinutes*60)-1;
+                timeStatusBreak();
+            } else {
+                document.getElementById("time").innerHTML = "Start Time";
+                timeleft = (sessionMinutes*60)-1;
+                timeStatusOn();
+            }
+        }
+
 }
+
+, 1000);
+}
+
+function timeStatusBreak(){
+    document.getElementById("time").style.color = "red";
+    document.getElementById("sessionText").innerHTML = "BREAK";
+    document.getElementById("sessionText").style.color = "red";
+    timeStatus = 1;
+    timeCountdown();
+}
+
+function timeStatusOn(){
+    document.getElementById("time").style.color = "black";
+    document.getElementById("sessionText").innerHTML = "TIME";
+    document.getElementById("sessionText").style.color = "black";
+    timeStatus = 0;
+    timeCountdown();
+}
+
+
+
+
+
+
+
+//Stop
+stop = document.getElementById("stop");
+stop.addEventListener("click", stopTimer);
+
+function stopTimer(){
+clearInterval(downTimer);
+document.getElementById("sTime").innerHTML = sessionMinutes + ":00";
+    document.getElementById("time").innerHTML = sessionMinutes + ":00";
+    timeleft = (sessionMinutes*60)-61;
+    play.addEventListener("click", timeCountdown);
+}
+
+//Pause
+pause = document.getElementById("pause");
+pause.addEventListener("click", pauseTimer);
+
+function pauseTimer(){
+clearInterval(downTimer);
+document.getElementById("time").innerHTML = Math.round(timeleft/60) + ":" + Math.round((timeleft%60)+1);
+play.addEventListener("click", timeCountdown);
+}
+
+//Replay
+replay = document.getElementById("replay");
+replay.addEventListener("click", replayTimer);
+
+function replayTimer(){
+clearInterval(downTimer);
+document.getElementById("sTime").innerHTML = sessionMinutes + ":00";
+document.getElementById("time").innerHTML = sessionMinutes + ":00";
+timeleft = (sessionMinutes*60)-61;
+timeCountdown();
+}
+
+
+
+/*
+    timeleft = 10;
+    document.getElementById("time").innerHTML = Math.round(timeleft/60) + ":" + Math.round(timeleft%60);
+    timeStatus = 1;
+    timeCountdown();
+
+   if (timeleft <= 0 && timeStatus == 1){
+        clearInterval(downTimer);
+        document.getElementById("time").style.color = "black"
+        document.getElementById("sessionText").innerHTML = "TIME"
+        document.getElementById("sessionText").style.color = "black"
+        document.getElementById("time").innerHTML = Math.round(timeleft/60) + ":" + Math.round(timeleft%60);
+        timeleft = 10;
+        timeStatus = 0;
+        timeCountdown();
+   } */
+
